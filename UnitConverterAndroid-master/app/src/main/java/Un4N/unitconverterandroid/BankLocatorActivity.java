9786ac675876ab0a6c91661f22ec560c862ab7de
Arjun Suvarna <arjunsuvarna.s@gmail.com>
2023-04-12 12:11:07 -0500

    bank locator backend

--- UnitConverterAndroid-master/app/src/main/java/Un4N/unitconverterandroid/BankLocatorActivity.java
+++ UnitConverterAndroid-master/app/src/main/java/Un4N/unitconverterandroid/BankLocatorActivity.java
@@ -2,10 +2,20 @@ package Un4N.unitconverterandroid;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.apptakk.http_request.HttpRequest;
+import com.apptakk.http_request.HttpRequestTask;
+import com.apptakk.http_request.HttpResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 //import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,7 +37,53 @@ public class BankLocatorActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String searchText = searchBox.getText().toString();
                 Toast.makeText(BankLocatorActivity.this, "Searching for banks near " + searchText, Toast.LENGTH_SHORT).show();
-                // TODO: Add code to search for banks near the entered location
+                new HttpRequestTask(
+                        new HttpRequest("https://pfabankapi.app.cloud.gov/api/locations?filters=ZIP:\\\""+searchText+"\\\"", HttpRequest.GET),
+                        new HttpRequest.Handler() {
+                            @Override
+                            public void response(HttpResponse response) {
+                                if (response.code == 200) {
+
+                                    JSONObject mainObject = null;
+                                    try {
+                                        mainObject = new JSONObject(response.body.toString());
+                                        JSONArray res = mainObject.getJSONArray("data");
+                                        JSONObject  data1 = res.getJSONObject(0);
+                                        JSONObject dataBank1 = data1.getJSONObject("data");
+                                        String bankName1 = dataBank1.getString("NAME");
+                                        String address1 = dataBank1.getString("ADDRESS");
+                                        TextView textbank1 = (TextView)findViewById(R.id.bank1);
+                                        textbank1.setText(bankName1);
+                                        TextView textaddress1 = (TextView)findViewById(R.id.address1);
+                                        textaddress1.setText(address1);
+                                        JSONObject  data2 = res.getJSONObject(1);
+                                        JSONObject dataBank2 = data2.getJSONObject("data");
+                                        String bankName2 = dataBank2.getString("NAME");
+                                        String address2 = dataBank2.getString("ADDRESS");
+                                        TextView textbank2 = (TextView)findViewById(R.id.bank2);
+                                        textbank2.setText(bankName2);
+                                        TextView textaddress2 = (TextView)findViewById(R.id.address2);
+                                        textaddress2.setText(address2);
+
+                                        JSONObject  data3 = res.getJSONObject(2);
+                                        JSONObject dataBank3 = data3.getJSONObject("data");
+                                        String bankName3 = dataBank3.getString("NAME");
+                                        String address3 = dataBank3.getString("ADDRESS");
+                                        TextView textbank3 = (TextView)findViewById(R.id.bank3);
+                                        textbank3.setText(bankName3);
+                                        TextView textaddress3 = (TextView)findViewById(R.id.address3);
+                                        textaddress3.setText(address3);
+                                        Log.d(this.getClass().toString(), "Request successful!" + bankName1 + address1);
+                                    } catch (JSONException e) {
+                                        throw new RuntimeException(e);
+                                    }
+
+
+                                } else {
+                                    Log.e(this.getClass().toString(), "Request unsuccessful: " + response);
+                                }
+                            }
+                        }).execute();
             }
         });
     }
